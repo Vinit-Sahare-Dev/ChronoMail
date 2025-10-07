ee660e6149b523f669b3f9ff69abac706d8275b3
Vinit-Sahare-Dev <vinit.sahare.dev@gmail.com>
2025-10-07 21:21:08 +0530

    Initial commit: Add email sender project

---
+++ src/main/java/com/email/sender/EmailModel/EmailRequest.java
@@ -0,0 +1,55 @@
+package com.email.sender.EmailModel;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class EmailRequest {
+    
+    @NotBlank(message = "Receiver's email is required")
+    @Email(message = "Invalid email format")
+    private String receiversMail;
+    
+    private String subject;
+    private String body;
+
+    public EmailRequest() {}
+
+    public EmailRequest(String receiversMail, String subject, String body) {
+        this.receiversMail = receiversMail;
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public String getReceiversMail() {
+        return receiversMail;
+    }
+
+    public void setReceiversMail(String receiversMail) {
+        this.receiversMail = receiversMail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    
+    @Override
+    public String toString() {
+        return "EmailRequest{" +
+                "receiversMail='" + receiversMail + '\'' +
+                ", subject='" + subject + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/email/sender/EMailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMailsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
